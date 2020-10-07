d7557262bdd5364c7596a7af3ea002423a6d6513
Felipe Gomes Belarmino <70665848+felipetrybe@users.noreply.github.com>
2020-10-07 19:51:05 -0300

    Alterei a linha 5

--- exercises/2.1/github1.js
+++ exercises/2.1/github1.js
@@ -2,4 +2,5 @@ let student = "euMesmo";
 let studentSkills = 10;
 javascript-updates
 //Vou ter muito sucesso na programação!
+//Assim espero Lucas, para nós e todos da turma 8. Felipe G. Belarmino.
 console.log("Sucesso!");
